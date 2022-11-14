d4496ca85d469ba75cc38b9afcadc81ec70aa315
Cobeasta <stopyloknok@gmail.com>
2022-11-14 05:13:39 -0600

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CanvasComponent } from './canvas/canvas.component';
+import { Obstacle } from './models/Obstacle';
+import {ScenarioService} from "./scenario.service";
+import {SocketioService} from "./socketio.service";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CanvasComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    ScenarioService,
+    SocketioService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
